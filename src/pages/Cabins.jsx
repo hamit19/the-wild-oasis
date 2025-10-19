4f80f08ddd81bc0351ad2863cd29d9362ac46d3f
Hamid-Hassani <hamidhassani119@gmail.com>
2025-10-19 17:47:00 +0330

    feat/add cabin api request

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  const [cabins, setCabins] = useState([]);
+
+  useEffect(() => {
+    getCabins().then((data) => setCabins([...data]));
+  }, []);
+
+  if (!cabins.length) return <p>Loading...</p>;
+
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
+    <Row type='horizontal'>
+      <img src={cabins[0]?.image} alt='tres' />
+      <Heading as='h1'>All cabins</Heading>
       <p>TEST</p>
     </Row>
   );
