bcf52533e76b737abd180e3e711e4f49d358dd41
Hamid-Hassani <hamidhassani119@gmail.com>
2025-10-25 12:23:45 +0330

    feat/add input component

---
+++ src/ui/Input.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  width: 100%;
+  background-color: var(--color-grey-0);
+  color: var(--color-grey-900);
+  border: 1px solid var(--color-grey-200);
+  border-radius: 10px;
+  padding: 1rem 1rem;
+  transition: ease 0.1s all;
+`;
+
+export default function Input() {
+  return <StyledInput />;
+}
