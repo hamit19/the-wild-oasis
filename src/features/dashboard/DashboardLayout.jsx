4f607558ca3d46f15a13bab100b1a3016c6dafac
Hamid-Hassani <hamidhassani119@gmail.com>
2025-12-11 16:51:09 +0330

    feat/add dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,14 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+export default function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div> PlaceHolders </div>
+      <div> PlaceHolders </div>
+      <div> PlaceHolders </div>
+      <div> PlaceHolders </div>
+    </StyledDashboardLayout>
+  );
+}
