5441d8e53a4d61c549a92b3bfc253f57b16a220b
Hamid-Hassani <hamidhassani119@gmail.com>
2025-12-16 17:44:23 +0330

    fix/ remove defalut login credintials

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import useLogin from "./hooks/useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("hamid@example.com");
-  const [password, setPassword] = useState("Hamid@12");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { login, isLogging } = useLogin();
 
