bd76b64760a406defd35a181332d5f2e0a372a02
Hamid-Hassani <hamidhassani119@gmail.com>
2025-12-16 16:21:23 +0330

    fix/ fix close and open bug in Menu compo

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -86,6 +86,7 @@ function Toggle({ id }) {
   const { open, close, openId, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
     openId === "" || openId !== id ? open(id) : close();
 
     const rect = e.target.closest("button").getBoundingClientRect();
@@ -105,7 +106,7 @@ function Toggle({ id }) {
 
 function List({ children, id }) {
   const { openId, position, close } = useContext(MenusContext);
-  const { ref } = useOutsideClick(close);
+  const { ref } = useOutsideClick(close, false);
 
   if (openId !== id) return null;
 
